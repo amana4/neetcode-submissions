e1d9ddb8e2b4b09334f189878328cf6fa7a2f6cf
amana4 <68304126+amana4@users.noreply.github.com>
2026-08-22 19:12:30 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,13 @@
+
+#include <algorithm>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       std::sort(s.begin(), s.end()); 
+       std::sort(t.begin(), t.end());
+
+       return s==t;
+
+    }
+};
