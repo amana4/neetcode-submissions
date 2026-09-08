adcfa9cea588338b50e9ba01b0e05672242eeb49
amana4 <68304126+amana4@users.noreply.github.com>
2026-09-07 23:02:57 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map<string, vector<string>> res;
+       for(string s: strs)
+       {
+          string sorted = s;
+          sort(sorted.begin(), sorted.end());
+          res[sorted].push_back(s);
+       }
+       vector<vector<string>> ans;
+       for(auto entry:res)
+       {
+         ans.push_back(entry.second);
+       }
+       return ans; 
+    }
+};
